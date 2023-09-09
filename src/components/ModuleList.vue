e15185965009f8c27e75dabf11fd9637fd584a41
Jing <1341241068@qq.com>
2023-09-09 15:04:44 +0800

    基础主页

---
+++ src/components/ModuleList.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="modules-containter">
+        <div class="modules-item">
+            <div class="mudules-innter">
+                <h2 class="modules-title">关键字过滤</h2>
+                <p class="modules-description">
+                    关键字过滤
+                </p>
+            </div>
+        </div>
+        <div class="modules-item">
+            <div class="mudules-innter">
+                <h2 class="modules-title">Coming...</h2>
+                <p class="modules-description">
+                    正在开发中...
+                </p>
+            </div>
+        </div>
+        <div class="modules-item">
+            <div class="mudules-innter">
+                <h2 class="modules-title">Coming...</h2>
+                <p class="modules-description">
+                    正在开发中...
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.modules-containter{
+    display: flex;
+    flex-wrap: wrap;
+    margin: -8px;
+    padding: 100px;
+    .modules-item{
+        display: block;
+        width: calc(100% / 3);
+        padding: 8px;
+        .mudules-innter{
+            padding: 24px;
+            border: 1px solid #202127;
+            border-radius: 12px;
+            height: 100%;
+            background-color: #202127;
+            transition: border-color .25s,background-color .25s;
+            &:hover{
+                border-color: #a8b1ff;;
+            }
+        }
+        .modules-title{
+            line-height: 24px;
+            font-size: 16px;
+            font-weight: 600;
+        }
+        .modules-description{
+            flex-grow: 1;
+            padding-top: 8px;
+            line-height: 24px;
+            font-size: 14px;
+            font-weight: 500;
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,30 @@
 <template>
   <main>
-    <!-- <TheWelcome /> -->
-    This is Home-view.
+    <h1 class="main-title">
+      <span class="title-span">A Tools</span>
+    </h1>
+    <ModuleList />
   </main>
 </template>
 
 <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
+import ModuleList from '../components/ModuleList.vue'
 </script>
+
+<style>
+.main-title {
+  line-height: 64px;
+  font-size: 56px;
+  color: transparent;
+  letter-spacing: -.4px;
+  font-weight: 800;
+  white-space: pre-wrap;
+  margin: 100px 0 0 100px;
+  .title-span{
+    background: linear-gradient( 120deg, #bd34fe 30%, #41d1ff );
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+</style>
